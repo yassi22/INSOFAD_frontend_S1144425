96b5b8f7983ff22736326706928ec97455a9690c
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-05-19 19:49:53 +0200

    product options prijs

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
 import { CartService } from '../../services/cart.service'; 
 import { ProductVariant } from '../../models/productvariant.model';
+import { Options } from '../../models/options.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -16,7 +17,9 @@ import { ProductVariant } from '../../models/productvariant.model';
 export class ProductDetailComponent {
 
   private productId: number; 
-  public selectedProductVariant: [ProductVariant | null , number | null ] = [null, null]; 
+  public selectedProductVariant: [ProductVariant | null , number | null ] = [null, null];  
+  public defaultprice: number = 0; 
+  
   
   @Input() public product!: Product;
   @Output() public onBuyProduct: EventEmitter<Product> = new EventEmitter<Product>();
@@ -37,27 +40,45 @@ export class ProductDetailComponent {
     .getProductByIndex(this.productId)
     .subscribe((product: Product) => {
       this.product = product; 
-      this.product.price = product.price; 
+      this.product.price = product.price;   
+      if(this.defaultprice == 0 ){ 
+        this.defaultprice = product.price; 
+        console.log("dit is een update van de prijs" + this.defaultprice);
+      } 
       console.log(product);
-    }); 
+      console.log(product.price);
+    });  
+    
+   
 
   }  
 
-  public addToPrice(additional_cost: number, productVariant:ProductVariant){  
-      this.selectedProductVariant.forEach((variant: any) => { 
-          if(variant.includes(productVariant)) {
-           let previous_additional_cost =  this.selectedProductVariant[1]; 
-           this.product.price -= previous_additional_cost ?? 0; 
-           this.product.price += additional_cost; 
-           variant[1] = additional_cost; 
-            return;  
-          } 
+  public addToPrice(additional_cost: number, productVariant:ProductVariant, productoptions:Options){    
+      console.log(productVariant); 
+      console.log(productoptions);     
+      console.log(this.defaultprice); 
+      
+      let options_cost = productoptions.added_price;   
+      
+      // de default prijs van product wordt opgeteld met de aangeklikte option. 
+      this.product.price =  this.defaultprice + options_cost; 
 
-          this.selectedProductVariant.push(productVariant); 
-          this.selectedProductVariant[1] = additional_cost;  
-          this.product.price += additional_cost; 
-      });
-      this.product.price += additional_cost; 
+
+
+      // this.selectedProductVariant.forEach((variant: any) => { 
+      //     if(variant.includes(productVariant)) {
+      //      let previous_additional_cost =  this.selectedProductVariant[1]; 
+      //      this.product.price -= previous_additional_cost ?? 0; 
+      //      this.product.price += additional_cost; 
+      //      variant[1] = additional_cost; 
+      //       return;  
+      //     } 
+
+      //     this.selectedProductVariant.push(productVariant); 
+      //     this.selectedProductVariant[1] = additional_cost;  
+      //     this.product.price += additional_cost; 
+      // });
+      // this.product.price += additional_cost; 
     
 
   } 
