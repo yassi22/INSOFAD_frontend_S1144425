3566f6dae186a3ab0b02e4db3161b703c8cbd132
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-06-06 16:39:20 +0200

    product variant WIP

--- src/app/admin/update-variant-option/update-variant-option.component.ts
+++ src/app/admin/update-variant-option/update-variant-option.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-update-variant-option',
   standalone: true,
-  imports: [],
+    imports: [
+        NgForOf,
+        ReactiveFormsModule
+    ],
   templateUrl: './update-variant-option.component.html',
   styleUrl: './update-variant-option.component.scss'
 })
