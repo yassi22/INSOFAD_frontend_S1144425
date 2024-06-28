e4bb2934e937b79fd128e3f6c878edd9fb5a96e0
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-06-28 14:59:17 +0200

    waring message in de terminal opgelost -- order id toegevoegd aan de orders

--- src/app/models/getorder.model.ts
+++ src/app/models/getorder.model.ts
@@ -2,7 +2,7 @@ import {Product} from "./product.model";
 import {OrderProduct} from "./orderproduct.model";
 
 export class GetOrder {
-    public id: number;  
+    public orderId: number;
     public datum: Date; 
     public orderTitle: string;
     public orderPrice: number;
